465acc053ed04a4204480591c1b82671d871147b
Charlie Clarke <claclark@gmail.com>
2024-06-03 04:40:59 +0000

    txt wrapper to translate JSON structure

---
+++ src/json_txt.cc
@@ -0,0 +1,85 @@
+#include "src/json_txt.h"
+
+#include <map>
+#include <memory>
+#include <string>
+
+#include "src/core.h"
+#include "src/recipe.h"
+#include "src/scribe.h"
+#include "src/txt.h"
+
+namespace cottontail {
+namespace {
+bool should_wrap(const std::map<std::string, std::string> &parameters) {
+  auto it = parameters.find("json");
+  if (it != parameters.end())
+    return okay(it->second);
+  else
+    return false;
+}
+
+bool should_wrap(const std::string &recipe) {
+  if (recipe == "json")
+    return true;
+  std::map<std::string, std::string> parameters;
+  if (cook(recipe, &parameters))
+    return should_wrap(parameters);
+  else
+    return false;
+}
+} // namespace
+
+std::shared_ptr<Txt> JsonTxt::wrap(const std::string &recipe,
+                                   std::shared_ptr<Txt> txt,
+                                   std::string *error) {
+  if (should_wrap(recipe))
+    return wrap(txt, error);
+  else
+    return txt;
+}
+
+std::shared_ptr<Txt>
+JsonTxt::wrap(const std::map<std::string, std::string> &parameters,
+              std::shared_ptr<Txt> txt, std::string *error) {
+  if (should_wrap(parameters))
+    return wrap(txt, error);
+  else
+    return txt;
+}
+
+std::shared_ptr<Txt> JsonTxt::wrap(std::shared_ptr<Txt> txt,
+                                   std::string *error) {
+  std::shared_ptr<JsonTxt> jtxt = std::shared_ptr<JsonTxt>(new JsonTxt());
+  assert(jtxt != nullptr);
+  jtxt->txt_ = txt;
+  return jtxt;
+}
+
+bool JsonTxt::transaction_(std::string *error) {
+  return txt_->transaction(error);
+}
+
+bool JsonTxt::ready_() { return txt_->ready(); }
+
+void JsonTxt::commit_() { txt_->commit(); }
+
+void JsonTxt::abort_() { txt_->abort(); }
+
+std::string JsonTxt::name_() { return txt_->name(); }
+
+std::string JsonTxt::recipe_() { return txt_->recipe(); }
+
+std::shared_ptr<Txt> JsonTxt::clone_(std::string *error) {
+  return wrap(txt_->clone(error));
+}
+
+std::string JsonTxt::translate_(addr p, addr q) {
+  return scribe_translate_json(txt_->translate(p, q));
+}
+
+addr JsonTxt::tokens_() { return txt_->tokens(); }
+
+bool JsonTxt::range_(addr *p, addr *q) { return txt_->range(p, q); }
+
+} // namespace cottontail
---
+++ src/json_txt.h
@@ -0,0 +1,45 @@
+#ifndef COTTONTAIL_SRC_JSON_TXT_H_
+#define COTTONTAIL_SRC_JSON_TXT_H_
+
+#include <map>
+#include <memory>
+#include <string>
+
+#include "src/core.h"
+#include "src/txt.h"
+
+namespace cottontail {
+class JsonTxt final : public Txt {
+public:
+  static std::shared_ptr<Txt> wrap(const std::string &recipe,
+                                   std::shared_ptr<Txt> txt,
+                                   std::string *error = nullptr);
+  static std::shared_ptr<Txt>
+  wrap(const std::map<std::string, std::string> &parameters,
+       std::shared_ptr<Txt> txt, std::string *error = nullptr);
+  static std::shared_ptr<Txt> wrap(std::shared_ptr<Txt> txt,
+                                   std::string *error = nullptr);
+
+  ~JsonTxt(){};
+  JsonTxt(const JsonTxt &) = delete;
+  JsonTxt &operator=(const JsonTxt &) = delete;
+  JsonTxt(JsonTxt &&) = delete;
+  JsonTxt &operator=(JsonTxt &&) = delete;
+
+private:
+  JsonTxt(){};
+  std::shared_ptr<Txt> txt_;
+  bool transaction_(std::string *error) final;
+  bool ready_() final;
+  void commit_() final;
+  void abort_() final;
+  std::string name_() final;
+  std::string recipe_() final;
+  std::shared_ptr<Txt> clone_(std::string *error) final;
+  std::string translate_(addr p, addr q) final;
+  addr tokens_() final;
+  bool range_(addr *p, addr *q) final;
+};
+} // namespace cottontail
+
+#endif // COTTONTAIL_SRC_JSON_TXT_H_
