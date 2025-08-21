977c861f58ffe8a9ca3bd87821eb3809ac2853b6
Charlie Clarke <claclark@gmail.com>
2025-08-21 15:07:24 +0000

    update sandbox app

--- apps/working.cc
+++ apps/working.cc
@@ -30,14 +30,38 @@
 #include <unistd.h>
 #include <vector>
 
+#include "meadowlark/meadowlark.h"
 #include "src/cottontail.h"
 
 void usage(std::string program_name) {
-  std::cerr << "usage: " << program_name << "\n";
+  std::cerr << "usage: " << program_name << "[--burrow burrow] \n";
 }
+
+TEST(Example, Nothing) {}
 
 int main(int argc, char **argv) {
   std::string program_name = argv[0];
   std::string error;
+  if (argc == 2 && argv[1] == std::string("--help")) {
+    usage(program_name);
+    return 0;
+  }
+  std::string burrow = cottontail::DEFAULT_BURROW;
+  while (argc > 1) {
+    if (argc > 2 &&
+        (argv[1] == std::string("-b") || argv[1] == std::string("--burrow"))) {
+      burrow = argv[2];
+      argc -= 2;
+      argv += 2;
+    } else {
+      usage(program_name);
+      return 1;
+    }
+  }
+  if (argc != 1) {
+    usage(program_name);
+    return 1;
+  }
+  test_Example_Nothing();
   return 0;
 }
